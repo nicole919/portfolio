f3fefd70c213c58f37348832f7cdb203dea3fe94
Nicole Fafard <nicole.fafard@gmail.com>
2019-08-28 21:33:57 -0700

    completed

---
+++ script.js
@@ -0,0 +1,12 @@
+$(() => {
+    $('#projectsLink').on('click', () => {
+      document.getElementById("projectsSection").scrollIntoView({behavior: 'smooth'});
+    })
+  });
+  
+  
+  $(() => {
+  $('#contactsLink').on('click', () => {
+  document.getElementById("contactSection").scrollIntoView({behavior: 'smooth'});
+    })
+  });
