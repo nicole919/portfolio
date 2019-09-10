9bed4cbff2bcd7e129bc8638c7f1f52a9afc85b2
nicole919 <52558643+nicole919@users.noreply.github.com>
2019-09-10 16:05:52 -0700

    media query update

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-$(() => {
-    $('#projectsLink').on('click', () => {
-      document.getElementById("projectsSection").scrollIntoView({behavior: 'smooth'});
-    })
-  });
-  
-  
-  //$(() => {
-  //$('#contactsLink').on('click', () => {
-  //document.getElementById("contactSection").scrollIntoView({behavior: 'smooth'});
-  //  })
-  //});
 
-  $(() => {
-    $('#bioLink').on('click', () => {
-      document.getElementById("bioSection").scrollIntoView({behavior: 'smooth'});
-    })
-  });
+$(() => {
+  $('#projectsLink').on('click', () => {
+    document.getElementById("projectsSection").scrollIntoView({behavior: 'smooth'});
+  })
+  $('#bioLink').on('click', () => {
+    document.getElementById("bioBio").scrollIntoView({behavior: 'smooth', block:'center'});
+  })
+
+
+  $('#hamburger').on('click',(e) => {
+    const menuLinks = $('#myLinks');
+    const hamburgerIcon = $('#hamburgerIcon');
+    if (menuLinks.css('display') === "flex") {
+      menuLinks.css('display', 'none');
+      hamburgerIcon.removeClass('fa-close');
+      hamburgerIcon.addClass('fa-bars');
+    } else {
+      menuLinks.css('display','flex');
+      hamburgerIcon.removeClass('fa-bars');
+      hamburgerIcon.addClass('fa-close');
+    }
+  })
+});
